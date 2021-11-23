cce4c7d529764a7b26c08c1599096c73f8959e6d
BRB3D <samuraix221@hotmail.com>
2021-11-23 16:39:39 -0500

    previous commit didnt included the alternative, this one does

--- 4_queries/name_of_teachers_that_assisted.sql
+++ 4_queries/name_of_teachers_that_assisted.sql
@@ -6,3 +6,11 @@ JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY teachers.name, cohorts.name
 HAVING cohorts.name = 'JUL02'
 ORDER BY teacher;
+
+/* SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher; */
